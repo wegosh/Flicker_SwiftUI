7e0d1df7d16f93b224ebaa8e37cea15aad396c31
Patryk Wegrzynski <wegosh16@gmail.com>
2023-05-27 18:33:38 +0100

    Initial Commit

---
+++ Flicker/FlickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickerApp.swift
+//  Flicker
+//
+//  Created by Patryk Wegrzynski Personal on 27/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
